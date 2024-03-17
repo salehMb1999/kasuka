fa8770323ef14f774b0d995b33d6e5a0af58951b
salehmb1999 <salehmirbagheri78@gmail.com>
2024-03-17 11:51:30 +0330

    add new font and prettier plugin for tailwindcss

--- src/header.jsx
+++ src/header.jsx
@@ -1,8 +1,10 @@
-export default function Header () {
-    return (
-        <div>
-        <h1 className="bg-red-500 text-3xl font-bold text-white">salam baby</h1>
-        <p>halet khube?</p>
+export default function Header() {
+  return (
+    <div>
+      <h1 className="font-iranSansLight bg-red-500 text-3xl font-bold text-white">
+        سلام بیبی
+      </h1>
+      <p>حالت خوبه عزیزم؟</p>
     </div>
-    )
+  );
 }
