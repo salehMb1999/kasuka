4160bf44020e6287769076be3a89d8b43c45d325
salehmb1999 <salehmirbagheri78@gmail.com>
2024-03-17 13:13:43 +0330

    finish header successfully 95%

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./header";
+import Header from "./header/header";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
