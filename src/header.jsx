431ebd667937ad779e79eb003d6b74878f824afc
salehmb1999 <salehmirbagheri78@gmail.com>
2024-03-17 11:10:48 +0330

    just initializing the project

---
+++ src/header.jsx
@@ -0,0 +1,8 @@
+export default function Header () {
+    return (
+        <div>
+        <h1 className="bg-red-500 text-3xl font-bold text-white">salam baby</h1>
+        <p>halet khube?</p>
+    </div>
+    )
+}
