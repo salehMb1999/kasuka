c3e2d2b55a01f99be8eb3e31699bbb2439316580
salehmb1999 <salehmirbagheri78@gmail.com>
2024-03-21 11:04:36 +0430

    finished counts section with responsive completely :)

--- src/counts/counts.jsx
+++ src/counts/counts.jsx
@@ -1,9 +1,9 @@
 export default function Counts() {
   return (
     <div className="container mx-auto px-3 py-20 font-iranSansMed">
-      <div className="flex items-start justify-between gap-10">
-        <div className="h-[500px] w-1/2 bg-[url('/src/public/images/counts-img.jpg')] bg-cover bg-center bg-no-repeat lg:w-1/2"></div>
-        <div className="flex w-1/2 flex-col justify-start gap-10 pt-5">
+      <div className="flex flex-col items-start justify-between gap-2 lg:flex-row lg:gap-10">
+        <div className="h-[500px] w-full bg-[url('/src/public/images/counts-img.jpg')] bg-cover bg-center bg-no-repeat lg:w-1/2"></div>
+        <div className="flex w-full flex-col justify-start gap-10 pt-5 lg:w-1/2">
           <div className="flex flex-col justify-start gap-3">
             <h2 className="text-3xl">
               شایسته ترین لذت را به هرحال ارائه میدهد
@@ -14,41 +14,41 @@ export default function Counts() {
               درد مقصر است
             </p>
           </div>
-          <div className="flex flex-wrap justify-start gap-10">
-            <div className="flex w-2/5 flex-col gap-2">
+          <div className="flex flex-col flex-wrap justify-between gap-14 md:flex-row md:gap-5">
+            <div className="flex w-full flex-col gap-3 md:w-2/5 md:gap-2">
               <div className="flex items-center justify-start gap-5 text-4xl">
                 <i className="fas fa-smile selected"></i>
                 <p>65</p>
               </div>
-              <p className="text-slate-700">
+              <p className="text-sm text-slate-700 lg:text-base">
                 مشتریان خوشحال خوشحالیم که از معمار چیزی دریافت کنیم.
               </p>
             </div>
-            <div className="flex w-2/5 flex-col gap-2">
+            <div className="flex w-full flex-col gap-3 md:w-2/5 md:gap-2">
               <div className="flex items-center justify-start gap-5 text-4xl">
                 <i className="fa fa-book selected" aria-hidden="true"></i>
                 <p>85</p>
               </div>
-              <p className="text-slate-700">
+              <p className="text-sm text-slate-700 lg:text-base">
                 پروژه ها برای بدست آوردن و زمانی که او کل کسانی را که از او
                 تعریف می کنند رد می کند و آنها را حقیر می شمارد
               </p>
             </div>
-            <div className="flex w-2/5 flex-col gap-2">
+            <div className="flex w-full flex-col gap-3 md:w-2/5 md:gap-2">
               <div className="flex items-center justify-start gap-5 text-4xl">
                 <i className="fas fa-clock selected"></i>
                 <p>35</p>
               </div>
-              <p className="text-slate-700">
+              <p className="text-sm text-slate-700 lg:text-base">
                 سال ها تجربه یا اجازه دهید به دنبال نوعی مزیت باشد
               </p>
             </div>
-            <div className="flex w-2/5 flex-col gap-2">
+            <div className="flex w-full flex-col gap-3 md:w-2/5 md:gap-2">
               <div className="flex items-center justify-start gap-5 text-4xl">
                 <i className="fas fa-award selected"></i>
                 <p>20</p>
               </div>
-              <p className="text-slate-700">
+              <p className="text-sm text-slate-700 lg:text-base">
                 جوایز درد دردناک تر است در زمانی دیگر که هیچکس او را متهم نمی
                 کند و هیچ وقت او را متهم نمی کند
               </p>
