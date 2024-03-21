9ce7ddf5921f611a0c238f2d14dd7d223a3cf447
salehmb1999 <salehmirbagheri78@gmail.com>
2024-03-21 13:13:28 +0430

    little update on teams component

--- src/teams/teams.jsx
+++ src/teams/teams.jsx
@@ -1,7 +1,7 @@
 import TeamMembers from "./teamMembers";
 export default function Teams() {
   return (
-    <div className="container mx-auto px-3 py-10 font-iranSansMed">
+    <div className="container mx-auto px-3 pb-20 pt-10 font-iranSansMed">
       <div className="flex flex-col items-start justify-start gap-3">
         <span className="relative text-slate-500 before:absolute before:right-8 before:top-1/2 before:h-0.5 before:w-32 before:-translate-y-1/2 before:bg-[#ffc451]">
           تیم
