0daf9be302ecffee815530db7b83288136f3037b
salehmb1999 <salehmirbagheri78@gmail.com>
2024-03-22 12:16:58 +0430

    teams props added to teams component

--- src/teams/teamMembers.jsx
+++ src/teams/teamMembers.jsx
@@ -1,22 +1,23 @@
 import TeamBoxes from "./teamBoxes";
-
-export default function TeamMembers() {
+import PropTypes from "prop-types";
+export default function TeamMembers(props) {
   return (
     <div className="group/box w-full overflow-hidden rounded-lg font-iranSansMed shadow-md shadow-slate-300 md:w-[calc(50%-1.25rem)] lg:w-[calc(25%-1.25rem)]">
       <div className="relative z-0 flex items-center justify-center overflow-hidden">
-        <img
-          className="h-full w-full"
-          src="./src/public/images/team-2.jpg"
-          alt="team-2"
-        />
+        <img className="h-full w-full" src={props.image} alt="team-2" />
         <div className="absolute bottom-10 left-1/2 z-50 flex w-full -translate-x-1/2 items-center justify-center opacity-0 duration-300 ease-in-out group-hover/box:bottom-5 group-hover/box:opacity-100">
           <TeamBoxes />
         </div>
       </div>
       <div className="p-5">
-        <p className="text-xl">سارا جونسون</p>
-        <p className="text-slate-400">مدیر تولید</p>
+        <p className="text-xl">{props.name}</p>
+        <p className="text-slate-400">{props.job}</p>
       </div>
     </div>
   );
 }
+TeamMembers.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  job: PropTypes.string.isRequired,
+};
--- src/teams/teams.jsx
+++ src/teams/teams.jsx
@@ -9,10 +9,26 @@ export default function Teams() {
         <h2 className="text-3xl">تیم ما را بررسی کنید</h2>
       </div>
       <div className="flex w-full flex-wrap items-center justify-between gap-5  pt-10">
-        <TeamMembers />
-        <TeamMembers />
-        <TeamMembers />
-        <TeamMembers />
+        <TeamMembers
+          image="src/public/images/team-1.jpg"
+          name="والتر وایت"
+          job="مدیر ارشد اجرایی"
+        />
+        <TeamMembers
+          image="src/public/images/team-2.jpg"
+          name="سارا جونسون"
+          job="مدیر تولید"
+        />
+        <TeamMembers
+          image="src/public/images/team-3.jpg"
+          name="ویلیام اندرسون"
+          job="CTO"
+        />
+        <TeamMembers
+          image="src/public/images/team-4.jpg"
+          name="آماندا جپسون"
+          job="حسابدار"
+        />
       </div>
     </div>
   );
