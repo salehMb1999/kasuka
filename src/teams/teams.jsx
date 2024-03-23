698ee7a89be8e4fd432db3d5c0d9a96c05740c15
salehmb1999 <salehmirbagheri78@gmail.com>
2024-03-23 10:21:57 +0430

    ad spread syntax to send props

--- src/teams/teams.jsx
+++ src/teams/teams.jsx
@@ -1,5 +1,31 @@
 import TeamMembers from "./teamMembers";
 export default function Teams() {
+  const allEmployees = [
+    {
+      id: 1,
+      image: "src/public/images/team-1.jpg",
+      name: "والتر وایت",
+      job: "مدیر ارشد اجرایی",
+    },
+    {
+      id: 2,
+      image: "src/public/images/team-2.jpg",
+      name: "سارا جونسون",
+      job: "مدیر تولید",
+    },
+    {
+      id: 3,
+      image: "src/public/images/team-3.jpg",
+      name: "ویلیام اندرسون",
+      job: "CTO",
+    },
+    {
+      id: 4,
+      image: "src/public/images/team-4.jpg",
+      name: "آماندا جپسون",
+      job: "حسابدار",
+    },
+  ];
   return (
     <div className="container mx-auto px-3 pb-20 pt-10 font-iranSansMed">
       <div className="flex flex-col items-start justify-start gap-3">
@@ -9,26 +35,10 @@ export default function Teams() {
         <h2 className="text-3xl">تیم ما را بررسی کنید</h2>
       </div>
       <div className="flex w-full flex-wrap items-center justify-between gap-5  pt-10">
-        <TeamMembers
-          image="src/public/images/team-1.jpg"
-          name="والتر وایت"
-          job="مدیر ارشد اجرایی"
-        />
-        <TeamMembers
-          image="src/public/images/team-2.jpg"
-          name="سارا جونسون"
-          job="مدیر تولید"
-        />
-        <TeamMembers
-          image="src/public/images/team-3.jpg"
-          name="ویلیام اندرسون"
-          job="CTO"
-        />
-        <TeamMembers
-          image="src/public/images/team-4.jpg"
-          name="آماندا جپسون"
-          job="حسابدار"
-        />
+        <TeamMembers {...allEmployees[0]} />
+        <TeamMembers {...allEmployees[1]} />
+        <TeamMembers {...allEmployees[2]} />
+        <TeamMembers {...allEmployees[3]} />
       </div>
     </div>
   );
